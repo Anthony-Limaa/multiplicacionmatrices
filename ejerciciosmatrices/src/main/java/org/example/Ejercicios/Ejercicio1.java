20a4ef6fbe192b4f0fd915807b1eeef55ef32b2f
Tony-Limaa <125584909+Anthony-Limaa@users.noreply.github.com>
2024-04-16 19:52:39 -0600

    Add files via upload

---
+++ ejerciciosmatrices/src/main/java/org/example/Ejercicios/Ejercicio1.java
@@ -0,0 +1,41 @@
+package org.example.Ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Definir las matrices A y B
+        int[][] A = {{1, 2, 3}, {4, 5, 6}};
+        int[][] B = {{5, -1}, {1, 0}, {-2, 3}};
+
+        // Verificar si las matrices son multiplicables
+        int filasA = A.length;
+        int columnasA = A[0].length;
+        int filasB = B.length;
+        int columnasB = B[0].length;
+
+        if (columnasA != filasB) {
+            System.out.println("Las matrices no son multiplicables.");
+            return;
+        }
+
+        // Crear la matriz resultante C
+        int[][] C = new int[filasA][columnasB];
+
+        // Multiplicar las matrices A y B
+        for (int i = 0; i < filasA; i++) {
+            for (int j = 0; j < columnasB; j++) {
+                for (int k = 0; k < columnasA; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        // Imprimir la matriz resultante C
+        System.out.println("Matriz resultante C:");
+        for (int i = 0; i < filasA; i++) {
+            for (int j = 0; j < columnasB; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
